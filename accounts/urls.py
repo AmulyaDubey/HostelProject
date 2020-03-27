55f8daae68d16c156982d3b51d04350b3a53888e
Amulya <macbookpro@Amulyas-MacBook-Pro.local>
2020-03-27 22:48:29 +0530

    login_done

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
+from django.conf.urls import url
 from . import views
 from django.contrib.auth.views import LoginView
 
 urlpatterns=[
     path('', views.home, name="home"),
-    path('login/',  LoginView.as_view() , {'template_name': 'login.html'}),
-    path('details/<str:pk>/', views.details, name="details"),
+    path('login/', views.login),
+    path('details/', views.details, name="details"),
     path('room/', views.room_aval, name="room"),
-    path('confirm/', views.confirm, name="confirm")
+    path('confirm/<str:pk>/', views.confirm, name="confirm")
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,35 +1,46 @@
 from django.shortcuts import render, HttpResponse
-from django.contrib.auth.forms import UserCreationForm
-from accounts.models import Login
-from django.contrib.auth import authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from accounts.models import Login,Room
+from django.contrib.auth import login,logout, authenticate
 from django.contrib import messages
 
-def login( request ):
-    if request.method=='POST':
+def login( request):
+    if request.method =='POST':
+        print("HEELLOOOO")
         username=request.POST.get('username')
         password=request.POST.get('password')
+        print(username)
+        print(password)
         try:
+                 print("LOTTERRYY")
+                 user=Login.objects.get(roll=username)
+                 print(user)
+                 print(user.pass_field)
+                 if user.pass_field == password:
+                        context = {'user': user}
+                        return render(request, 'accounts/details.html', context)
+                 else:
+                        messages.info(request, 'Username OR password is incorrect')
 
-            user = Login.objects.get(roll = username)
-            if user.pass_field == password:
-                context={'user':user}
-                return render('/account/details', context)
-            else:
+        except:
                 messages.info(request, 'Username OR password is incorrect')
-
-
-        except Login.DoesNotExist:
-            return render('account/')
-
-    return render(request, 'login.html')
+    print("BYYYEEEE")
+    form=AuthenticationForm()
+    return render(request, "registration/login.html", {"form":form})
 
 def home( request ):
     return render(request, 'accounts/login.html')
+
+
 def details (request, pk ):
     user=Login.objects.get(id=pk)
     context={'user':user}
     return render(request, 'accounts/details.html', context)
+
 def room_aval ( request ):
-    return render( request, 'accounts/rooms.html')
-def confirm(request):
-    return render( request, 'accounts/confirm.html')
+    return render( request, 'accounts/rooms.html',)
+
+def confirm(request, pk):
+    user = Room.objects.get(number=pk)
+    context = {'user': user}
+    return render( request, 'accounts/confirm.html',context)
